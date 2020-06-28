c8b1b129eec34c49142bbf95536dccba96361b7b
David Wiles <davidwiles33@gmail.com>
2020-06-28 13:59:58 -0500

    Move all generic code into goplai, only configuration files remain

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module roguelike-v1
 go 1.14
 
 require (
+	github.com/david-wiles/goplai v0.0.2-0.20200628185015-903aa2defe9f
 	github.com/faiface/pixel v0.9.0
-	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7
 )
--- main.go
+++ main.go
@@ -1,93 +1,31 @@
 package main
 
 import (
+	"github.com/david-wiles/goplai"
 	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/pixelgl"
 	_ "image/png"
-	"time"
 )
 
-// Game loop which conducts the game. This represents each 'tick' of the world
-func gameLoop() {
-	last := time.Now()
-	for !global.gWin.Closed() {
-		dt := time.Since(last).Seconds()
-		last = time.Now()
-
-		global.gController.Update(dt)
-		global.gCamera.Update(dt)
-		global.gWorld.Draw(dt)
-
-		global.gWin.Update()
-	}
-}
-
-// Sets up all global variables and objects
-func setup() {
-	global.gTextures.Load(wTexturePath)
-
-	global.gWorld.Init()
-	global.gWorld.LoadMap()
-
-	// Initialize player
-	global.gPlayer.Init(&CompoundSprite{
-		batch: global.gTextures.batch,
-		sprites: []struct {
-			sprite *pixel.Sprite
-			matrix pixel.Matrix
-		}{
-			{
-				sprite: global.gTextures.sprites["gold_knight"].Frame,
-				matrix: pixel.IM,
-			},
-		},
-	}, &Physics{
-		Bounds: &Bounds{
-			X:      48,
-			Y:      21,
-			Width:  16,
-			Height: 16,
-			entity: global.gPlayer,
-		},
-		Center:   pixel.ZV,
-		Velocity: pixel.ZV,
-		entity:   global.gPlayer,
-	})
-	global.gWorld.qt.Insert(global.gPlayer.Bounds())
-
-	global.gCamera.Init()
-	global.gCamera.SetFollow(global.gPlayer)
-
-	global.gController.Init()
-	global.gHud.Init()
-	global.gMainMenu.Init()
-}
-
-// Function which runs execution of the program in window
-// Everything in this loop will be run on the main thread.
-// It is only necessary that graphics code run on this thread
-func run() {
-	global.gVariables.Load(wConfigFile)
-
-	// Initialize window
-	cfg := pixelgl.WindowConfig{
-		Title:  wWindowTitle,
-		Bounds: pixel.R(0, 0, global.gVariables.WindowWidth, global.gVariables.WindowHeight),
-		VSync:  true,
-	}
-	gWin, err := pixelgl.NewWindow(cfg)
-	if err != nil {
-		panic(err)
-	}
-	gWin.SetCursorVisible(false)
-	global.gWin = gWin
-
-	setup()
-
-	gameLoop()
-}
-
-// Entry point for the program
 func main() {
-	pixelgl.Run(run)
+	goplai.Run(func() {
+		goplai.InitGlobal(goplai.Config{
+			WindowTitle: "Welcome to the Dungeon",
+			VarPath:     "resources/game-config.json",
+		})
+		goplai.G.Textures.Load("resources/textures/dungeon-config.json")
+		goplai.G.World.LoadMap("resources/maps/map-config.json")
+
+		goplai.SetPlayer(goplai.NewMob(goplai.G.Textures.GetSprite("gold_knight"), &goplai.Physics{
+			Bounds: &goplai.Bounds{
+				X:      48,
+				Y:      21,
+				Width:  16,
+				Height: 16,
+			},
+			Center:   pixel.ZV,
+			Velocity: pixel.ZV,
+		}))
+
+		goplai.BeginSimulation()
+	})
 }
